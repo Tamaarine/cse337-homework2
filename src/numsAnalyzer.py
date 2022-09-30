635fcb927f5bfab52213be9e27fd244d3f128247
Tamaarine <irebootplaygt@gmail.com>
2022-09-30 10:46:30 -0400

    Finished countMissingValues

--- src/numsAnalyzer.py
+++ src/numsAnalyzer.py
@@ -5,7 +5,11 @@ def replaceMissingValues(x):
     pass
 
 def countMissingValues(x, k=0):
-    pass
+    if k >= x.ndim or k < -x.ndim:
+        raise ValueError
+    
+    A = np.isnan(x) # boolean mask on nan values. True if it is nan, False if it isn't
+    return A.sum(axis=k) # Return basically the number of nan on the specified k axis
 
 def exams_with_median_gt_K(x, k):
     pass
