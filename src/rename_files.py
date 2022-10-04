5d4913c1f42800648a9931dd1dc74864f032ec9a
Tamaarine <irebootplaygt@gmail.com>
2022-10-04 17:29:03 -0400

    Handed the renaming correctly, refractored the code, also handle the cases when the destination we are renaming into is an existing directory, just throw the error back to the user

--- src/rename_files.py
+++ src/rename_files.py
@@ -5,23 +5,38 @@ import os
 def rename(path):
     files = os.listdir(path)
     
+    # File format we are fixing is fixed so we can make the regex pattern
+    p = re.compile(r'snap\d{3}.txt')
+    
     # Filters out all the non-files
     files = [file for file in files if not os.path.isdir(os.path.join(path, file))]
     
-    # Filters out only .txt files
-    files = [file for file in files if file.endswith(".txt")]
+    # Filter out only the snap files
+    files = [file for file in files if p.match(file) is not None]
     
     files.sort()
     
-    # File format we are fixing is fixed
-    p = re.compile(r'snap\d{3}.txt')
+    if len(files):
+        txt_index = files[0].find(".txt")
+        counter = int(files[0][:txt_index][-3:])
     
-    counter = 1
     for file in files:
         if p.match(file) is not None:
             txt_index = file.find(".txt")
             if counter != int(file[:txt_index][-3:]):
                 old_full_path = os.path.join(path, file)
                 new_full_path = os.path.join(path, f"snap{counter:03}.txt")
-                os.rename(old_full_path, new_full_path)
+                
+                # Re-raise the same error that's caught back to the user
+                try:
+                    os.rename(old_full_path, new_full_path)
+                except FileExistsError as e:
+                    # For windows, if dst exists not matter what it is
+                    # FileExistsError will be raised
+                    raise e
+                except IsADirectoryError as e:
+                    # src is a file
+                    # dst is a directory
+                    # incompatible move
+                    raise e
             counter += 1
